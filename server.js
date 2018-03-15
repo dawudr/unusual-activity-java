bbc9771c9b5cc0367d9318dad38e275ac78bda79
Dawud Rahman <dawudr@gnmail.com>
2018-03-15 23:03:34 +0000

    Fix  Node.js: package.json not found in application root

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// const path = require('path');
+const path = require('path');
 // const request = require('request');
 const express = require('express');
 const app = express();
-// const server = require('http').Server(app);
+const server = require('http').Server(app);
 
 // app.use(function (req, res, next) {
 ////   res.header('Access-Control-Allow-Origin', '*');
@@ -66,4 +66,11 @@ app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
+
+// PathLocationStrategy
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Deployed and console listening!');
