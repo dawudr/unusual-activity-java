c8c567cb1bc44df4679165ef6f271ace32da1c84
Dawud Rahman <dawudr@gnmail.com>
2018-05-08 13:43:36 +0200

    Integrate with new stats table

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/unusualactivity
-export SPRING_DATASOURCE_USERNAME=root
-export SPRING_DATASOURCE_PASSWORD=password
+#!/usr/bin/env bash
+#export SPRING_DATASOURCE_URL=jdbc:mysql://localhost:3306/unusualactivity
+#export SPRING_DATASOURCE_USERNAME=root
+#export SPRING_DATASOURCE_PASSWORD=password
+export SPRING_DATASOURCE_URL=jdbc:sqlserver://fjdb.centralus.cloudapp.azure.com:1433;database=UA
+export SPRING_DATASOURCE_USERNAME=devuser
+export SPRING_DATASOURCE_PASSWORD=devuser
 mvn spring-boot:run
