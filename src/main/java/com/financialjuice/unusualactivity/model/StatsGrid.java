05e951f80fb093376414d51aaa78a5096f01cede
Dawud Rahman <dawudr@gnmail.com>
2018-06-02 11:12:01 +0100

    Change News field from Varchar to Text Mysql 8

--- src/main/java/com/financialjuice/unusualactivity/model/StatsGrid.java
+++ src/main/java/com/financialjuice/unusualactivity/model/StatsGrid.java
@@ -33,7 +33,7 @@ public class StatsGrid implements Serializable {
     private long latestVolume;
     @Column(name="name", length=100)
     private String name;
-    @Column(name="news", length=20000)
+    @Column(name="news", length = 65535, columnDefinition = "text")
     private String news;
     @Basic
     @Temporal(TemporalType.DATE)
